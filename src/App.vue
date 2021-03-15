c80f090cd4ee0360f2f84a947d8d40ffe99c5010
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-15 09:49:36 -0500

    vuex setup complete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<template> </template>
+<template>
+  <div id="app">
+    <h1>Movie Vuer</h1>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style></style>
