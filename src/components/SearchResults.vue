48258c5418521cb799968586229e7d4115eff4d2
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-15 10:23:02 -0500

    wip

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="searchTerm">
+    <h3>
+      Found {{ searchResults.total_results }} results for "{{ searchTerm }}"
+    </h3>
+    <div v-for="movie in searchResults.results" :key="movie.id">
+      {{ movie.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["searchResults", "searchTerm"]),
+  },
+};
+</script>
+
+<style></style>
