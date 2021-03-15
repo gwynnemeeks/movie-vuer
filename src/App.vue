5e9768757705fb4f5e0e26b5f857c4ce9d219729
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-15 10:33:09 -0500

    store and components set up

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar app color="purple" light> </v-app-bar>
-
-    <v-main>
-      <v-container>
-        <search-results />
-      </v-container>
-    </v-main>
+    <v-app-bar app color="purple" dark>
+      <h1>Movie Vuer</h1>
+      <form @submit.prevent="handleSubmit" style="background: white">
+        <input type="text" v-model="query" />
+      </form>
+    </v-app-bar>
   </v-app>
 </template>
 
 <script>
-import SearchResults from "./components/SearchResults";
-
 export default {
-  components: {
-    SearchResults,
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log("Search for:", this.query);
+    },
   },
 };
 </script>
