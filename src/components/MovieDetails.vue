40e17ae929ab7761511613a192096d6d9ad7c48d
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-15 11:10:11 -0500

    wip details added

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="movieDetails">
+    <p>{{ movieDetails.title }}</p>
+    <p>{{ movieDetails.backdrop_path }}</p>
+    <p>{{ movieDetails.runtime }}</p>
+    <p>{{ movieDetails.release_date }}</p>
+    <p>{{ movieDetails.overview }}</p>
+    <ul>
+      <li v-for="genre in movieDetails.genres" :key="genre.id">
+        {{ genre.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["movieDetails"]),
+  },
+};
+</script>
+
+<style></style>
