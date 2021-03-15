d7f31752e988421b7069b6587e4f16ba51c51aea
Gwynne Meeks <meeks.gwynne@gmail.com>
2021-03-15 10:57:26 -0500

    wip dispatching actions

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   data() {
     return {
@@ -17,8 +19,11 @@ export default {
     };
   },
   methods: {
+    ...mapActions(["searchMovies"]),
+
     handleSubmit() {
       console.log("Search for:", this.query);
+      this.searchMovies(this.query);
     },
   },
 };
